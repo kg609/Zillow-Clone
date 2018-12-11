4f692639b315cf5994916ee8a16972e243ba2e94
Kiwane Garner <kiwanegarner@Kiwanes-MacBook-Pro.local>
2018-12-11 10:46:09 -0500

    first commit for this project

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => {
+     return (
+         <nav>
+             <ul>
+                 <li>Buy</li>
+                 <li>Rent</li>
+                 <li>Sell</li>
+                 <li>Mortgages</li>
+                 <li>Agent finder</li>
+                 <li>More</li>
+                 
+             </ul>
+         </nav>
+     );
+};
+
+export default Navbar;
