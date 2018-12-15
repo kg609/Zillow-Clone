0e5dbfe40c77a2b03271f497ea22e078daa2aa71
Kiwane Garner <kiwanegarner@Kiwanes-MacBook-Pro.local>
2018-12-14 19:13:27 -0500

    Added more content to search bar component.

--- src/components/Search-Bar.js
+++ src/components/Search-Bar.js
@@ -8,7 +8,7 @@ class SearchBar extends React.Component {
                 <h1>Find your way home</h1>
 
                 <ul className="CTA-Links">
-                    <li><a href="placeholder">Buy</a></li>
+                    <li className="active"><a href="placeholder">Buy</a></li>
                     <li><a href="placeholder">Rent</a></li>
                     <li><a href="placeholder">Sell</a></li>
                     <li><a href="placeholder">Zestimate</a></li>
@@ -16,8 +16,14 @@ class SearchBar extends React.Component {
 
                 <form className="searchForm">
                     <input type="text" placeholder="Enter an address neighborhood, city, or ZIP code" name="search"/>
-                    <button className="search-button">Search</button>
+                    <button type="submit" className="search-button">Search</button>
                 </form>
+
+                <section className="banner">
+                    <i class="fa fa-users fa-2x" aria-hidden="true"></i>
+                    <h4>Be ready to buy Get pre-qualified by a local lender on zillow.</h4>
+                    <button className="start-Btn">Get Started</button>
+                </section>
             </div>
         );
     };
